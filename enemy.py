94b58c586351473c49478f2fbeb501ce6709bf75
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-07-04 00:12:37 -0300

    name correction

--- enemy.py
+++ enemy.py
@@ -1,5 +1,5 @@
 from stateMachine import Entity
-from states import State
+from enetityStates import State
 class Enemy(Entity):
     def __init__(self, state: State, lp: int, dmg: int, m_dmg: int, mp: int) -> None:
         super().__init__(state, lp, dmg, m_dmg, mp)
